787aa40bed50600fa8a509cce910f217c64da98b
pdrofelicio-prog <pedro.felicio.silva@gmail.com>
2024-09-02 17:28:30 -0300

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import IMCForm from './components/IMCForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calculadora de IMC</h1>
+      <IMCForm />
+    </div>
+  );
+}
+
+export default App;
